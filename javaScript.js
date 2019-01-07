726e9f65f0762e3c862aacde8cdcdd0746f1b0d1
Jakob Kennerberg <jakobkennerberg98@gmail.com>
2019-01-07 12:16:19 +0100

    heh

--- javaScript.js
+++ javaScript.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
 
 //Latitude: -90 to +90, toFixed() decides the amount of decimals
 function getCoordinatesLat() {
-  return (Math.random() * (-90 - 90) + 90).toFixed(3) * 1;
+  return (Math.random() * (-90 - 90) + 90).toFixed(7) * 1;
 }
 //Longitude: -180 to +180, toFixed() decides the amount of decimals
 function getCoordinatesLong() {
-  return (Math.random() * (-180 - 180) + 180).toFixed(3) * 1;
+  return (Math.random() * (-180 - 180) + 180).toFixed(7) * 1;
 }
