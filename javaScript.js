b5ade3806a3b636bc5f716de1ed76740d7a5e3bf
Yun <yunfah.chow@gmail.com>
2019-01-07 12:50:23 +0100

    fick lite error, kan ha fuckat det dock

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,13 @@
 var county;
-var
+var;
 $(document).ready(function() {
-}
+});
 
 //Latitude: -90 to +90, toFixed() decides the amount of decimals
 function getCoordinatesLat() {
   return (Math.random() * (-90 - 90) + 90).toFixed(7) * 1;
-}
+};
 //Longitude: -180 to +180, toFixed() decides the amount of decimals
 function getCoordinatesLong() {
   return (Math.random() * (-180 - 180) + 180).toFixed(7) * 1;
-}
+};
